f02020f257a233b09cbfa536e29abcbc3897c45e
DungHo <dung.hoduy@asnet.com.vn>
2017-08-31 16:08:12 +0700

    [#3] Config model to apply acts_as_bookable

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 
   after_initialize :set_default_role, :if => :new_record?
-  has_many :bookings
+  acts_as_booker
 
   def set_default_role
     self.role ||= :staff
--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -6,6 +6,8 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  schedule   :text
+#  capacity   :integer
 #
 
 class RoomSerializer < ActiveModel::Serializer
