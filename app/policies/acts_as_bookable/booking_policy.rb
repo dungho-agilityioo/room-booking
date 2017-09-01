5fff1cbd1bef0f8be98c7d7b77c39182aa7f7cf5
DungHo <dung.hoduy@asnet.com.vn>
2017-09-01 15:43:37 +0700

    [#3] Add role for booking api

---
+++ app/policies/acts_as_bookable/booking_policy.rb
@@ -0,0 +1,19 @@
+class ActsAsBookable::BookingPolicy < ApplicationPolicy
+
+  def index?
+    login?
+  end
+
+  def create?
+    login?
+  end
+
+  # def update?
+  #   @current_user.admin?
+  # end
+
+  def destroy?
+    login?
+  end
+
+end
