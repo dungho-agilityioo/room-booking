d1279491fed65be10167d71b726047f1fcbc2e55
DungHo <dung.hoduy@asnet.com.vn>
2017-09-01 22:23:59 +0700

    [#3] Refactor validate booking model

--- app/models/acts_as_bookable/booking_decorator.rb
+++ app/models/acts_as_bookable/booking_decorator.rb
@@ -12,8 +12,6 @@ ActsAsBookable::Booking.class_eval do
   end
 
   def limit_time
-    time_start_limit = DateTime.new(self.time_start.year, self.time_start.month, self.time_start.day, 7, 30)
-    time_end_limit = DateTime.new(self.time_end.year, self.time_end.month, self.time_end.day, 17, 30)
 
     if self.time_start < time_start_limit || self.time_end > time_end_limit
       raise (
@@ -27,5 +25,13 @@ ActsAsBookable::Booking.class_eval do
         )
       )
     end
+  end
+
+  def time_start_limit
+    DateTime.new(self.time_start.year, self.time_start.month, self.time_start.day, 7, 30)
+  end
+
+  def time_end_limit
+    DateTime.new(self.time_end.year, self.time_end.month, self.time_end.day, 17, 30)
   end
 end
