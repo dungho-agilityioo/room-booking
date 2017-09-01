c646aa505457176b967cdf62935d4c72a6a833b0
DungHo <dung.hoduy@asnet.com.vn>
2017-09-01 22:23:37 +0700

    [#3] Implement test case for a submit the room booking api

--- spec/controllers/api/v1/room_bookings_controller_spec.rb
+++ spec/controllers/api/v1/room_bookings_controller_spec.rb
@@ -35,22 +35,112 @@ RSpec.describe Api::V1::RoomBookingsController, type: :controller do
       end
     end
 
-    # context 'when the record do not exists' do
-    #   let(:room_booking_id) { 1000 }
+    context 'when the record do not exists' do
+      let(:room_booking_id) { 1000 }
 
-    #   it { should respond_with(404) }
+      it { should respond_with(404) }
 
-    #   it 'return a not found message' do
-    #     expect(response.body).to match(/Couldn't find /)
-    #   end
-    # end
+      it 'return a not found message' do
+        expect(response.body).to match(/Couldn't find /)
+      end
+    end
 
   end
 
+  describe 'POST /room_bookings' do
+    let(:valid_attributes) do
+      FactoryGirl.attributes_for(:room_booking)
+        .merge( time_start: Date.today.next_week + 1.day + 8.hours )
+        .merge( time_end: Date.today.next_week + 1.day + 10.hours )
+        .merge( room_id: room.id )
+    end
+
+    context 'when the request is valid' do
+
+      before { post :create, params: valid_attributes }
+
+      it { should respond_with(201) }
+
+      it 'should be reserve the room' do
+        expect(json["attributes"]["time-start"].to_datetime).to eq( Date.today.next_week + 1.day + 8.hours )
+        expect(json["attributes"]["time-end"].to_datetime).to eq( Date.today.next_week + 1.day + 10.hours - 1.second )
+      end
+
+      it 'should be booker is user' do
+        expect(json['relationships']['booker']['data']['id'].to_i).to eq(user.id)
+      end
+
+      it 'should be bookable is room' do
+        expect(json['relationships']['bookable']['data']['id'].to_i).to eq(room.id)
+      end
+    end
+
+    context 'when the request is missing the title' do
+      before { post :create, params: { } }
+
+      it { should respond_with(400) }
+
+      it 'return a validation failure message' do
+        expect(response.body).to match(/Parameter title is required/)
+      end
+    end
+
+    context 'when the request is missing the room' do
+      before { post :create, params: FactoryGirl.attributes_for(:room_booking) }
+
+      it { should respond_with(400) }
+
+      it 'return a validation failure message' do
+        expect(response.body).to match(/Parameter room_id is required/)
+      end
+    end
+
+    context 'when the request is missing the start date' do
+      before do
+        post :create,
+              params: FactoryGirl.attributes_for(:room_booking)
+                        .merge(room_id: room.id)
+                        .except(:time_start)
+      end
+
+      it { should respond_with(400) }
+
+      it 'return a validation failure message' do
+        expect(response.body).to match(/Parameter time_start is required/)
+      end
+    end
+
+    context 'when the request is missing the end date' do
+      before do
+        post :create,
+              params: FactoryGirl.attributes_for(:room_booking)
+                        .merge(room_id: room.id)
+                        .except(:time_end)
+      end
+
+      it { should respond_with(400) }
+
+      it 'return a validation failure message' do
+        expect(response.body).to match(/Parameter time_end is required/)
+      end
+    end
+
+  end
 
   describe 'DELETE /room_bookings/:id' do
-    before { delete :destroy, params: { id: room_booking_id } }
 
-    it { should respond_with(204) }
+    context 'when room booking is exists' do
+      before { delete :destroy, params: { id: room_booking_id } }
+
+      it { should respond_with(204) }
+    end
+
+    context 'when room booking do not exists' do
+      before { delete :destroy, params: { id: 1000 } }
+
+      it 'return a not found message' do
+        expect(response.body).to match(/Couldn't find Room Booking/)
+      end
+    end
   end
 end
