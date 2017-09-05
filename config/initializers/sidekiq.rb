ee6d1c189741070df733ef74f4986e4b72e61942
DungHo <dung.hoduy@asnet.com.vn>
2017-09-05 16:49:30 +0700

    [#3] Change config the redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 # schedule_file = "config/schedule.yml"
 
 Sidekiq.configure_server do |config|
-  redis_host = ENV.fetch('REDIS_HOST','localhost')
-  config.redis = { url: "redis://"+redis_host+":6379/0" }
+  redis_url = ENV.fetch('REDIS_URL','redis://redis:6379/0')
+  config.redis = { url: redis_url }
 
   schedule_file = File.expand_path("../../../config/scheduler.yml",__FILE__)
 
@@ -12,6 +12,6 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  redis_host = ENV.fetch('REDIS_HOST','localhost')
-  config.redis = { url: "redis://"+redis_host+":6379/0" }
+  redis_url = ENV.fetch('REDIS_URL','redis://redis:6379/0')
+  config.redis = { url: redis_url }
 end
