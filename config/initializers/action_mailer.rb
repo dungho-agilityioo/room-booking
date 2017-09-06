83c0cb58b3560e20f3febed7961f059129d1f0b6
DungHo <dung.hoduy@asnet.com.vn>
2017-09-06 10:35:41 +0700

    [#3] Update config for action mailer

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,19 +1,16 @@
-if Rails.env.development?
-  ActionMailer::Base.raise_delivery_errors = true
-  ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "gmail.com",
-    :user_name            => ENV['SMTP_EMAIL'],
-    :password             => ENV['SMTP_PASSWORD'],
-    :authentication       => 'plain',
-    :enable_starttls_auto => true,
-    :openssl_verify_mode  => 'none'
-  }
-
-  ActionMailer::Base.default_url_options = {
-    host: 'localhost:3000'
-  }
-end
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => ENV['SMTP_EMAIL'],
+  :password             => ENV['SMTP_PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true,
+  :openssl_verify_mode  => 'none'
+}
+ActionMailer::Base.default_url_options = {
+  host: ENV["SMTP_HOST"]
+}
