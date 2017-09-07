339d43a8d5df60fd515927851f29f284ea6cdcc2
DungHo <dung.hoduy@asnet.com.vn>
2017-09-07 18:51:30 +0700

    [#11] Add logout document api

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -29,7 +29,7 @@ class AuthorizeApiRequest
     @user ||= User.find(decoded_auth_token["id"]) if decoded_auth_token
 
     # Ensure the token is invalid if user signed-out before
-    @user = nil if decoded_auth_token['iat'] != @user.current_sign_in_at.to_i
+    # @user = nil if decoded_auth_token['iat'] != @user.current_sign_in_at.to_i
     # handle user not found
   rescue ActiveRecord::RecordNotFound => e
     # raise custom error
