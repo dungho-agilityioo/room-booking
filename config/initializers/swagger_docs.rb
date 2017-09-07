b11245202e9e4576451c32bb147362a0713fb87d
DungHo <dung.hoduy@asnet.com.vn>
2017-09-07 14:12:37 +0700

    [#11] Refactor struct folder for swagger UI

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -3,24 +3,31 @@ Swagger::Docs::Config.register_apis({
     # the extension used for the API
     :api_extension_type => :json,
     # the output location where your .json files are written to
-    :api_file_path => "public/api/v1/",
-    :controller_base_path => "api/v1",
+    :api_file_path => "public/docs/",
+    # :controller_base_path => "api/v1",
     # the URL base path to your API
-    # :base_path => ENV['BASE_PATH'],
-    # # if you want to delete all .json files at each generation
+    :base_path => '/',
+    # if you want to delete all .json files at each generation
     # :clean_directory => false,
-    # # Ability to setup base controller for each api version. Api::V1::SomeController for example.
+    # Ability to setup base controller for each api version. Api::V1::SomeController for example.
     # :parent_controller => ApplicationController,
-    # # add custom attributes to api-docs
+    # add custom attributes to api-docs
     :attributes => {
       :info => {
         "title" => "Room Booking Api App",
-        "description" => "Agility IO micro app: Rooms Booking."
+        "description" => "Agility IO micro app: Rooms Booking.",
         # "termsOfServiceUrl" => "http://helloreverb.com/terms/",
-        # "contact" => "apiteam@wordnik.com",
+        "contact" => "dung.hoduy@asnet.com.vn",
         # "license" => "Apache 2.0",
         # "licenseUrl" => "http://www.apache.org/licenses/LICENSE-2.0.html"
       }
     }
   }
 })
+
+class Swagger::Docs::Config
+  def self.transform_path(path, api_version)
+    # Make a distinction between the APIs and API documentation paths.
+    "docs/#{path}"
+  end
+end
