841839f5ea329dc3695ae0a2f44cda2ebc7dcbde
DungHo <dung.hoduy@asnet.com.vn>
2017-09-13 21:25:08 +0700

    [#10] combined report by date and by project into one

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -3,52 +3,44 @@ module Api
     class ReportsController < ApplicationController
       swagger_controller :room, "Room Management"
 
-      # POST reports/range_date
+      # GET reports?
       # :nocov:
-      swagger_api :by_range_date do |api|
+      swagger_api :index do |api|
         summary "Fetches all Rooms Booking in the range time"
         param :query, :page, :integer, :optional, "Page Number"
-        Api::V1::ReportsController::add_common_params(api)
+        param_list :query, :type, :string, :required, "Type", [:date, :project]
+        api.param :query, :room_id, :integer, :optional, "Room Id"
+        api.param :query, :project_id, :integer, :optional, "Project Id"
+        api.param :query, :time_start, :DateTime, :optional, "Time Start"
+        api.param :query, :time_end, :DateTime, :optional, "Time End"
         response :ok, "Success", :Room
         response :unauthorized
       end
       # :nocov:
-      def by_range_date
+      def index
         authorize self
 
+        param! :type, String, required: true
         param! :page, Integer
-        param! :room_id, Integer, required: true
-        param! :time_start, DateTime, required: true
-        param! :time_end, DateTime, required: true
 
+        if params[:type] == 'date'
+          param! :room_id, Integer, required: true
+          param! :time_start, DateTime, required: true
+          param! :time_end, DateTime, required: true
+        else
+          param! :project_id, Integer, required: true
+        end
         page = params[:page].present? && params[:page] || 1
-        total = ReportService.by_range_date(params).count
 
-        room_bookings = ReportService.by_range_date(params).page(page)
+        if params[:type] == 'date'
+          total = ReportService.by_range_date(params).count
 
-        respone_collection_serializer(room_bookings, page, total)
-      end
+          room_bookings = ReportService.by_range_date(params).page(page)
+        else
+          total = ReportService.by_project(params[:project_id]).count
 
-      # POST reports/by_project
-      # :nocov:
-      swagger_api :by_project do |api|
-        summary "Fetches all Rooms Booking by project id"
-        param :query, :page, :integer, :optional, "Page Number"
-        param :form, :project_id, :integer, :required, "Project Id"
-        response :ok, "Success", :Room
-        response :unauthorized
-      end
-      # :nocov:
-      def by_project
-        authorize self
-
-        param! :page, Integer
-        param! :project_id, Integer, required: true
-
-        page = params[:page].present? && params[:page] || 1
-        total = ReportService.by_project(params[:project_id]).count
-
-        room_bookings = ReportService.by_project(params[:project_id]).page(page)
+          room_bookings = ReportService.by_project(params[:project_id]).page(page)
+        end
 
         respone_collection_serializer(room_bookings, page, total)
       end
--- app/policies/api/v1/reports_controller_policy.rb
+++ app/policies/api/v1/reports_controller_policy.rb
@@ -1,10 +1,6 @@
 class Api::V1::ReportsControllerPolicy < ApplicationPolicy
 
-  def by_range_date?
-    @current_user.admin?
-  end
-
-  def by_project?
+  def index?
     @current_user.admin?
   end
 
