3e739f24c7f544fe4da64ca02d19bfaa218f443c
DungHo <dung.hoduy@asnet.com.vn>
2017-09-14 08:37:28 +0700

    [#3] Refactor to change controller to namespace admin

---
+++ app/controllers/api/v1/admin/rooms_controller.rb
@@ -0,0 +1,108 @@
+module Api
+  module V1
+    module Admin
+      class RoomsController < ApplicationController
+      	before_action :find_room, only: [:show, :update, :destroy]
+        swagger_controller :room, "Room Management"
+
+        # GET /rooms
+        # :nocov:
+        swagger_api :index do
+          summary "Fetches all Rooms"
+          response :ok, "Success", :Room
+          response :unauthorized
+          response :not_found
+        end
+        # :nocov:
+        def index
+          @rooms = Room.all
+          authorize Room
+          json_response(@rooms)
+        end
+
+        # GET /rooms/:id
+        # :nocov:
+        swagger_api :show do
+          summary "Fetches a single Room item"
+          param :path, :id, :integer, :required, "Room Id"
+          response :ok, "Success", :Room
+          response :unauthorized
+          response :not_found
+        end
+        # :nocov:
+        def show
+          authorize @room
+          json_response(@room)
+        end
+
+
+        # POST /rooms
+        # :nocov:
+        swagger_api :create do
+          summary "Creates a new Room"
+          param :form, :name, :string, :required, "Room Name"
+          response :created, "Success", :Room
+          response :unauthorized
+        end
+        # :nocov:
+        def create
+          @room = Room.new(room_params)
+          authorize @room
+          if @room.save
+            json_response(@room, :created)
+          else
+            json_response(@room.errors.full_messages, :unprocessable_entity)
+          end
+        end
+
+        # PUT /rooms/:id
+        # :nocov:
+        swagger_api :update do
+          summary "Update a Room"
+          param :path, :id, :integer, :required, "Room Id"
+          param :form, :name, :string, :required, "Room Name"
+          response :ok, "Success", :Room
+          response :unauthorized
+          response :not_found
+        end
+        # :nocov:
+        def update
+          authorize @room
+          if @room.update(room_params)
+            json_response(@room, :ok)
+          else
+            json_response(@room.errors.full_messages, :unprocessable_entity)
+          end
+        end
+
+        # DELETE /rooms/:id
+        # :nocov:
+        swagger_api :destroy do
+          summary "Delete a Room"
+          param :path, :id, :integer, :required, "Room Id"
+          response :no_content, "Success", :Room
+          response :unauthorized
+          response :not_found
+        end
+        # :nocov:
+        def destroy
+          authorize @room
+          @room.destroy
+          json_response( nil, :no_content)
+        end
+
+        private
+
+        def room_params
+          # whitelist params
+          params.permit(:name)
+        end
+
+        def find_room
+          @room = Room.find(params[:id])
+        end
+
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, skip: [:registrations, :sessions, :passwords, :comfirmations]
 
-  api_version(module: "Api::V1", path: { value: "api/v1" }, defaults: { format: :json }, default: true) do
+  api_version(module: "Api::V1", path: { value: "api/v1" }, defaults: { format: :json }) do
     devise_for :users, skip: [:registrations, :passwords],
       path_names: { sign_in: "login", sign_out: "logout" },
       controllers: {
@@ -11,23 +11,23 @@ Rails.application.routes.draw do
 
     devise_scope :user do
       get "/users/auth/gitlab/callback", to: "/api/v1/users/omniauth_callbacks#gitlab"
-      delete "users/logout", to: "users/omniauth_callbacks#destroy"
+      delete "logout", to: "users/omniauth_callbacks#destroy"
     end
 
     resources :room_bookings, except: [:update], :path => "books" do
-      post "search", on: :collection, action: "search"
+      post "search", on: :collection, action: :search
     end
 
     get 'projects', to: '/api/v1/users#projects'
 
-    scope 'admin', as: 'admin' do
+    namespace :admin do
       resources :projects
       resources :rooms
-      get "reports", to: "/api/v1/reports#index"
+      resources :reports, except: [:create, :update, :destroy, :show]
 
       resources :user_projects, except: [:update, :destroy, :show] do
-        put ':project_id', on: :collection, action: "update"
-        delete ':project_id', on: :collection, action: "destroy"
+        put ':project_id', on: :collection, action: :update
+        delete ':project_id', on: :collection, action: :destroy
       end
     end
 
