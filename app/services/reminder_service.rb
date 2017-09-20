404782f43a29cc0bc94b2112fb38ad2ef5f543a8
DungHo <dung.hoduy@asnet.com.vn>
2017-09-20 17:43:58 +0700

    [#15] Add get list booking to send mail reminder api

--- app/services/reminder_service.rb
+++ app/services/reminder_service.rb
@@ -1,14 +1,5 @@
 class ReminderService
   class << self
-    def send_mail(time)
-      room_bookings = booked_remider(time)
-
-      if room_bookings.present?
-        room_bookings.each do |booking|
-          UserMailer.reminder(booking).deliver_now
-        end
-      end
-    end
 
     def booked_remider(time)
       ActsAsBookable::Booking
