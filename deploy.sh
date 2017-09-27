da07ac65ffc4d4a1ecfc6a47d20ee679962ad1e2
DungHo <dung.hoduy@asnet.com.vn>
2017-09-27 17:36:09 +0700

    [#16] Install gcloud latest version instead of 120.0.0

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${GOOGLE_PROJECT_ID}/rooms-booking-api:$CIRCLE_SHA1
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch -f deploy/staging/09-api.yml -p '{"spec":{"template":{"spec":{"containers":[{"name":"api","image":"asia.gcr.io/rooms-booking-177603/rooms-booking-api:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch -f deploy/staging/09-api.yml -p '{"spec":{"template":{"spec":{"containers":[{"name":"api","image":"asia.gcr.io/'"$GOOGLE_PROJECT_ID"'/rooms-booking-api:'"$CIRCLE_SHA1"'"}]}}}}'
