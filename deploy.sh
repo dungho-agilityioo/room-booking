7752922619855bfbebc92d992cd3ffd4cd5cfa2a
DungHo <dung.hoduy@asnet.com.vn>
2017-09-27 16:06:23 +0700

    [#16] Change name image with project id instead of project name

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${GOOGLE_PROJECT_NAME}/rooms-booking-api:latest
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${GOOGLE_PROJECT_ID}/rooms-booking-api:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch -f deploy/staging/09-api.yml -p '{"spec":{"template":{"spec":{"containers":[{"name":"api","image":"asia.gcr.io/rooms-booking-177603/rooms-booking-api:latest"}]}}}}'
