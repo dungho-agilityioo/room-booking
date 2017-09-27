78dde8f1c764203c44590dd240b18962bd043ddf
DungHo <dung.hoduy@asnet.com.vn>
2017-09-27 15:28:35 +0700

    [#16] Always push latest for image

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@
 # Exit on any error
 set -e
 
-SHA1=$1
-
-sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${GOOGLE_PROJECT_NAME}/rooms-booking-api:$SHA1
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${GOOGLE_PROJECT_NAME}/rooms-booking-api:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch -f deploy/staging/09-api.yml -p '{"spec":{"template":{"spec":{"containers":[{"name":"api","image":"asia.gcr.io/rooms-booking-177603/rooms-booking-api:'"$SHA1"'"}]}}}}'
+kubectl patch -f deploy/staging/09-api.yml -p '{"spec":{"template":{"spec":{"containers":[{"name":"api","image":"asia.gcr.io/rooms-booking-177603/rooms-booking-api:latest"}]}}}}'
