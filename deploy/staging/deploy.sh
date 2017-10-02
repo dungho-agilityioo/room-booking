11a05b0d68444793d9e438cf564a5a55c1c60a7b
DungHo <dung.hoduy@asnet.com.vn>
2017-10-02 11:06:18 +0700

    [#16] Add namespace param in the kubectl command

--- deploy/staging/deploy.sh
+++ deploy/staging/deploy.sh
@@ -8,7 +8,7 @@ export IMAGE=asia.gcr.io/$GOOGLE_PROJECT_ID/rooms-booking-api:$CIRCLE_SHA1
 echo "Deploying $IMAGE"
 
 # Clean any old deploy-tasks jobs
-kubectl delete job api-migration-job 2> /dev/null || true
+kubectl delete job api-migration-job -n rb-staging 2> /dev/null || true
 
 # Create new deploy-tasks jobs
 cat deploy/staging/13-deploy-tasks-job.yml.tmpl | envsubst | kubectl apply -f -
@@ -22,7 +22,7 @@ done
 
 echo '=============== deploy_tasks output'
 if [[ `kubectl get pods -n rb-staging -a --selector="name=api-migration-job" -o 'jsonpath={.items[0].status.phase}'` != 'Succeeded' ]]; then
-  kubectl attach $(kubectl get pods -n rb-staging -a --selector="name=api-migration-job" -o 'jsonpath={.items[0].metadata.name}')
+  kubectl attach $(kubectl get pods -n rb-staging -a --selector="name=api-migration-job" -o 'jsonpath={.items[0].metadata.name}') -n rb-staging
 fi
 echo '==============='
 
@@ -32,8 +32,8 @@ while true; do
   if [[ "$phase" == "Succeeded" ]]; then
     break
   elif [[ "$phase" == "Failed" ]]; then
-    kubectl describe job api-migration-job
-    kubectl delete job api-migration-job
+    kubectl describe job api-migration-job -n rb-staging
+    kubectl delete job api-migration-job -n rb-staging
     echo "Deploy canceled. Deploy tasks failed!!!"
     exit 1
   fi
@@ -42,5 +42,5 @@ done
 # Deployment
 kubectl patch -f deploy/staging/09-api.yml -p '{"spec":{"template":{"spec":{"containers":[{"name":"api","image":'"$IMAGE"'}]}}}}'
 kubectl describe -f deploy/staging/09-api.yml
-kubectl delete job api-migration-job || true
+kubectl delete job api-migration-job  -n rb-staging || true
 kubectl rollout status -f deploy/staging/09-api.yml
