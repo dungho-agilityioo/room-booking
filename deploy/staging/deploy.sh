2eb75cdde839985e12ad1b182ac989aebba526c1
DungHo <dung.hoduy@asnet.com.vn>
2017-10-02 09:46:15 +0700

    [#16] Add command to check status migration job

--- deploy/staging/deploy.sh
+++ deploy/staging/deploy.sh
@@ -3,20 +3,44 @@
 # Exit on any error
 set -e
 
-apt-get install kubectl
+export IMAGE=asia.gcr.io/$GOOGLE_PROJECT_ID/rooms-booking-api:$CIRCLE_SHA1
 
-# Config and authentication google cloud
-echo $GOOGLE_AUTH | base64 --decode -i > ${HOME}/gcloud-service-key.json
+echo "Deploying $IMAGE"
 
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-gcloud --quiet config set project $GOOGLE_PROJECT_ID
-gcloud --quiet config set container/cluster $GOOGLE_CLUSTER_NAME
+# Clean any old deploy-tasks jobs
+kubectl delete job api-migration-job 2> /dev/null || true
 
-# Reading the zone from the env var is not working so we set it here
-gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
-gcloud --quiet container clusters get-credentials $GOOGLE_CLUSTER_NAME
+# Create new deploy-tasks jobs
+cat deploy/staging/13-deploy-tasks-job.yml.tmpl | envsubst | kubectl apply -f -
 
-# Push image to google cloud
-gcloud docker -- push asia.gcr.io/${GOOGLE_PROJECT_ID}/rooms-booking-api:$CIRCLE_SHA1
-chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch -f deploy/staging/09-api.yml -p '{"spec":{"template":{"spec":{"containers":[{"name":"api","image":"asia.gcr.io/'"$GOOGLE_PROJECT_ID"'/rooms-booking-api:'"$CIRCLE_SHA1"'"}]}}}}'
+while true; do
+  phase=`kubectl get pods -n rb-staging -a --selector="name=api-migration-job" -o 'jsonpath={.items[0].status.phase}' || 'false'`
+  if [[ "$phase" != 'Pending' ]]; then
+    break
+  fi
+done
+
+echo '=============== deploy_tasks output'
+if [[ `kubectl get pods -n rb-staging -a --selector="name=api-migration-job" -o 'jsonpath={.items[0].status.phase}'` != 'Succeeded' ]]; then
+  kubectl attach $(kubectl get pods -n rb-staging -a --selector="name=api-migration-job" -o 'jsonpath={.items[0].metadata.name}')
+fi
+echo '==============='
+
+while true; do
+  phase=`kubectl get pods -n rb-staging -a --selector="name=api-migration-job" -o 'jsonpath={.items[0].status.phase}'`
+  echo "Deploy task job status: $phase"
+  if [[ "$phase" == "Succeeded" ]]; then
+    break
+  elif [[ "$phase" == "Failed" ]]; then
+    kubectl describe job api-migration-job
+    kubectl delete job api-migration-job
+    echo "Deploy canceled. Deploy tasks failed!!!"
+    exit 1
+  fi
+done
+
+# Deployment
+kubectl patch -f deploy/staging/09-api.yml -p '{"spec":{"template":{"spec":{"containers":[{"name":"api","image":'"$IMAGE"'}]}}}}'
+kubectl describe -f deploy/staging/09-api.yml
+kubectl delete job api-migration-job || true
+kubectl rollout status -f deploy/staging/09-api.yml
