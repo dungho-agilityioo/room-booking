7ddf38257047a59075615be161ae993be237bb8b
DungHo <dung.hoduy@asnet.com.vn>
2017-10-12 17:18:17 +0700

    [#35] Remove filter report by project

--- api/app/services/report_service.rb
+++ api/app/services/report_service.rb
@@ -2,17 +2,14 @@ class ReportService
 
   class << self
     def by_range_date(params)
-      Booking
+      bookings = Booking
         .includes(:bookable, :booker)
-        .where(bookable_id: params[:room_id])
         .where("time_start >= ?", params[:time_start].to_datetime)
         .where("time_end <= ?", params[:time_end].to_datetime)
-    end
 
-    def by_project(project_id)
-      Booking
-        .includes(:bookable, :booker)
-        .where(project_id: project_id)
+      bookings = bookings.where(bookable_id: params[:room_id]) if params[:room_id].present?
+
+      bookings
     end
 
     def get_booked(time_start, time_end)
