c011e93cb7121b506bca854f1c88710cd5f39efb
DungHo <dung.hoduy@asnet.com.vn>
2017-10-16 17:15:01 +0700

    [#47] Fix test case for move check available to the booking resource

--- api/spec/controllers/api/v1/bookings_controller_spec.rb
+++ api/spec/controllers/api/v1/bookings_controller_spec.rb
@@ -3,28 +3,112 @@ require 'rails_helper'
 RSpec.describe Api::V1::BookingsController, type: :controller do
 
   let!(:user) { create(:user) }
-  let!(:room) { create(:room) }
-  let!(:booking) { create(:booking, room: room, user: user ) }
-  let(:booking_id) { booking.id }
 
   # authorize request
   let(:headers) { valid_headers }
 
-
   before(:each) { request.headers["Authorization"] = headers["Authorization"] }
 
   describe 'GET /books' do
-    before(:each) { get :index }
 
-    it { should respond_with(200) }
+    context '#getall' do
+      let!(:room) { create(:room) }
+      let!(:booking) { create(:booking, room: room, user: user ) }
+      before(:each) { get :index }
 
-    it 'returns list Bookings' do
-      expect(json).not_to be_empty
-      expect(json.size).to eq(1)
+      it { should respond_with(200) }
+
+      it 'returns list Bookings' do
+        expect(json).not_to be_empty
+        expect(json.size).to eq(1)
+      end
+    end
+
+    context '#filter' do
+      before(:all) do
+        room = create(:room)
+        15.times do |t|
+          create(
+              :booking_all,
+              room: room,
+              start_date: Date.today.next_week + t.hours,
+              end_date: Date.today.next_week + t.hours + 30.minutes
+            )
+        end
+      end
+      context '#booked' do
+
+        context 'when the request valid' do
+
+          context '#limit, #offset is blank' do
+            before {
+              get :index, params: {
+                filter: 'booked',
+                start_date: Date.today.next_week + 3.hours,
+                end_date: Date.today.next_week + 7.hours
+              }
+            }
+            it { should respond_with(200) }
+            specify { expect(json.size).to eq(5) }
+            specify { expect(metadata['total']).to eq(5) }
+          end
+
+          context '#limit, #offset is present' do
+            before {
+              get :index, params: {
+                filter: 'booked',
+                start_date: Date.today.next_week + 3.hours,
+                end_date: Date.today.next_week + 15.hours,
+                limit: 7,
+                offset: 4
+              }
+            }
+
+            it { should respond_with(200) }
+            specify { expect(json.size).to eq(7) }
+            specify { expect(metadata['total']).to eq(12) }
+          end
+        end
+
+        context 'when the request invalid' do
+          before { get :index, params: { filter: 'booked' } }
+
+          it { should respond_with(400) }
+
+          specify { expect(response.body).to match(/Parameter start_date is required/) }
+        end
+      end
+
+      context '#available' do
+        context 'when the request valid' do
+          before {
+            get :index, params: {
+              filter: 'available',
+              start_date: Date.today.next_week + 3.hours,
+              end_date: Date.today.next_week + 7.hours
+            }
+          }
+          it { should respond_with(200) }
+          specify { expect(json.size).to eq(4) }
+        end
+
+        context 'when the request invalid' do
+          before { get :index, params: { filter: 'available' } }
+
+          it { should respond_with(400) }
+
+          specify { expect(response.body).to match(/Parameter start_date is required/) }
+        end
+      end
     end
   end
 
+
+
   describe 'GET /books/:id' do
+    let!(:room) { create(:room) }
+    let!(:booking) { create(:booking, room: room, user: user ) }
+    let(:booking_id) { booking.id }
     before { get :show, params: { id: booking_id } }
 
     context 'when the record exists' do
@@ -48,6 +132,8 @@ RSpec.describe Api::V1::BookingsController, type: :controller do
   end
 
   describe 'POST /books' do
+    let!(:room) { create(:room) }
+    let!(:booking) { create(:booking, room: room, user: user ) }
     let(:valid_attributes) do
       FactoryGirl.attributes_for(:booking)
         .merge( start_date: Date.today.next_week + 1.day + 8.hours )
@@ -131,7 +217,9 @@ RSpec.describe Api::V1::BookingsController, type: :controller do
   end
 
   describe 'DELETE /books/:id' do
-
+    let!(:room) { create(:room) }
+    let!(:booking) { create(:booking, room: room, user: user ) }
+    let(:booking_id) { booking.id }
     context 'when Booking is exists' do
       before { delete :destroy, params: { id: booking_id } }
 
