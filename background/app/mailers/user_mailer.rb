4b2832106a4be5ee2096a52bf8a94be911075b9a
DungHo <dung.hoduy@asnet.com.vn>
2017-10-16 11:29:46 +0700

    Remove the code do not use

--- background/app/mailers/user_mailer.rb
+++ background/app/mailers/user_mailer.rb
@@ -2,19 +2,6 @@ require File.join(File.dirname(__FILE__), "../../config/require_all.rb")
 
 class UserMailer < ApplicationMailer
 
-  def room_booking(object = {})
-    @user_name = object["user_name"]
-    @room_name = object["room_name"]
-    @title = object["title"]
-    @start_date = object["start_date"].to_datetime.strftime('%m-%d-%Y %H-%M')
-    @end_date = object["end_date"].to_datetime.strftime('%m-%d-%Y %H-%M')
-    @daily = object["daily"]
-
-    mail to: ENV['ADMIN_EMAIL'], subject: "[Room Booking] Submit Room Booking" do |format|
-      format.html
-    end
-  end
-
   def reminder(object = {})
     @title = object["title"]
     @room_name = object["room"]["name"]
