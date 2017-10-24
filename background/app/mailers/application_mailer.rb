d4a035c3d37cfd3d0946b1801f3bb0ffe5fd3bfd
DungHo <dung.hoduy@asnet.com.vn>
2017-10-24 17:58:55 +0700

    [#52] Do not use mailer when send mail in background job

--- background/app/mailers/application_mailer.rb
+++
@@ -1,6 +0,0 @@
-require File.join(File.dirname(__FILE__), "../../config/require_all.rb")
-
-class ApplicationMailer < ActionMailer::Base
-  default from: "Room Booking <#{ENV['ADMIN_EMAIL']}>"
-  # layout 'mailer'
-end
