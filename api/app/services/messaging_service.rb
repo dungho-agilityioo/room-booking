a6055985c3e5534683e7029dcca30e0035474e68
DungHo <dung.hoduy@asnet.com.vn>
2017-10-24 16:59:52 +0700

    [#52] Add publish delayed message to send email reminder before 10 minute

--- api/app/services/messaging_service.rb
+++ api/app/services/messaging_service.rb
@@ -1,34 +1,61 @@
 class MessagingService
   include Singleton
+  DELAYED_REMINDER_QUEUE = 'amqp.bookings.reminder.later'
+  DESTINATION_REMINDER_QUEUE = 'amqp.bookings.reminder.now'
+  DESTINATION_BOOKING_QUEUE = 'amqp.bookings.after'
 
   def initialize
+    @connection ||= Bunny.new
+    @connection.start
     @exchange = channel.default_exchange
   end
 
   def channel
-    @connection ||= Bunny.new
-    @connection.start
-    @channel ||= @connection.create_channel
+    @channel ||= connection.create_channel
+  end
+
+  def connection
+    @connection
+  end
+
+  def exchange
+    @exchange
   end
 
   def queue
-    @queue ||= channel.queue('email_queue')
+    @queue ||= channel.queue(DESTINATION_BOOKING_QUEUE, :durable => true)
+  end
+
+  def delayed_squeue(expires_time)
+
+    # declare a queue with the DELAYED_REMINDER_QUEUE name
+    channel.queue(DELAYED_REMINDER_QUEUE, arguments: {
+      # set the dead-letter exchange to the default queue
+      'x-dead-letter-exchange' => '',
+      # when the message expires, set change the routing key into the destination queue name
+      'x-dead-letter-routing-key' => DESTINATION_REMINDER_QUEUE,
+      # the time in milliseconds to keep the message in the queue
+      'x-message-ttl' => expires_time
+    })
   end
 
   def publish(data)
-    @exchange.publish(data, :routing_key => queue.name)
-    @connection.close
+    exchange.publish(data, routing_key: queue.name)
   end
 
-  # def receive
-  #   begin
-  #     @queue.subscribe do |delivery_info, metadata, payload|
-  #       puts "Sending email..."
-  #       UserMailer.room_booking(payload).deliver_later
-  #     end
-  #   rescue Interrupt => _
-  #     $rabbitmq_connection.close
-  #   end
-  # end
+  def publish_delayed(data)
+    expires_time = get_expires_time(data)
 
+    delayed_squeue(expires_time)
+    exchange.publish(data, routing_key: DELAYED_REMINDER_QUEUE)
+  end
+
+  private
+
+  def get_expires_time(data)
+    booking = JSON.parse(data)
+    start_date = booking["start_date"].to_time - 10.minutes
+
+    (((start_date - Time.now) / 1.minutes).ceil) * 1000
+  end
 end
